24f5dd988cb44a26583eb18f00135f48b18c8195
UrbanismoPMRO <urbanismo.pmro@gmail.com>
2025-12-22 11:03:49 -0300

    Add files via upload

---
+++ styles/Lotes_2_style.js
@@ -0,0 +1,46 @@
+var size = 0;
+var placement = 'point';
+
+var style_Lotes_2 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    
+    var labelText = ""; 
+    var value = feature.get("var value = '';");
+    var labelFont = "13.0px \'Open Sans\', sans-serif";
+    var labelFill = "#fdf532";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = "left";
+    var offsetX = 0;
+    var offsetY = 0;
+    var placement = 'point';
+    if (feature.get("Name") !== null) {
+        labelText = String(feature.get("Name"));
+    }
+    
+        function rules_Lotes_2(feature, value) {
+            var context = {
+                feature: feature,
+                variables: {}
+            };
+            // Start of if blocks and style check logic
+            if (exp_Lotes_2rule0_eval_expression(context)) {
+                      return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(253,234,35,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 3.8}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    }
+            else {
+                return [];
+            }
+        }
+        var style = rules_Lotes_2(feature, value);
+        ;
+
+    return style;
+};
---
+++ styles/Novacamadaderascunho_14_style.js
@@ -0,0 +1,31 @@
+var size = 0;
+var placement = 'point';
+
+var style_Novacamadaderascunho_14 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    
+    var labelText = ""; 
+    var value = feature.get("");
+    var labelFont = "13.0px \'Open Sans\', sans-serif";
+    var labelFill = "#ff7f00";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = "left";
+    var offsetX = 0;
+    var offsetY = 0;
+    var placement = 'point';
+    if (feature.get("Zona") !== null) {
+        labelText = String(feature.get("Zona"));
+    }
+    var style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,0.489)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(145,82,45,0.489)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+
+    return style;
+};
