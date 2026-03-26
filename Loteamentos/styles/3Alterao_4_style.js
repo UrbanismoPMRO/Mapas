f566f8ada24d614571001e65f27e32125be361c3
UrbanismoPMRO <urbanismo.pmro@gmail.com>
2026-03-26 14:14:49 -0300

    Add files via upload

---
+++ Loteamentos/styles/3Alterao_4_style.js
@@ -0,0 +1,46 @@
+var size = 0;
+var placement = 'point';
+
+var style_3Alterao_4 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    
+    var labelText = ""; 
+    var value = feature.get("var value = '';");
+    var labelFont = "13.0px \'Open Sans\', sans-serif";
+    var labelFill = "#00ffff";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = "left";
+    var offsetX = 0;
+    var offsetY = 0;
+    var placement = 'point';
+    if (feature.get("Name") !== null) {
+        labelText = String(feature.get("Name"));
+    }
+    
+        function rules_3Alterao_4(feature, value) {
+            var context = {
+                feature: feature,
+                variables: {}
+            };
+            // Start of if blocks and style check logic
+            if (exp_3Alterao_4rule0_eval_expression(context)) {
+                      return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(0,255,255,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 1.9}),fill: new ol.style.Fill({color: 'rgba(0,255,255,0.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    }
+            else {
+                return [];
+            }
+        }
+        var style = rules_3Alterao_4(feature, value);
+        ;
+
+    return style;
+};
